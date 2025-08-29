f9ea65211506289d426f31abb02628a6e68f9800
Ubuntu <macinsight@macinsight.hiss0mu3xy0evpyk3bod4qyjea.dx.internal.cloudapp.net>
2025-08-29 13:31:19 +0000

    Fixes for CORS

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
+
+
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://mac-insight.vercel.app', 'https://j-mac.vercel.app', 'https://mac-insight-dev.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.set("timeout", 600000);
+
+
 const excelRoutes = require("./routes/excelRoutes");
 const templateRoutes = require("./routes/templateRoutes");
 const headerRoutes = require("./routes/headerRoutes");
@@ -12,11 +27,6 @@ const ruleRoutes = require("./routes/ruleRoutes");
 const revertRoutes = require("./routes/revertRoutes");
 const visualDataRoutes = require("./routes/populationRoutes");
 const referencesRoutes = require("./routes/crossReferenceRoutes");
-
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
-app.use(express.json({ limit: '100mb' }));
-app.use(cors());
-app.set("timeout", 600000);
 
 
 app.use("/api", excelRoutes);
