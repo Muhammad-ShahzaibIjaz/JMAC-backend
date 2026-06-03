34a6c88b37269d31871842775a7033b352488e1e
Muhammad-ShahzaibIjaz <muhammadshahzaibijaz34@gmail.com>
2026-06-03 13:57:01 +0500

    Updartes

--- controllers/operationController.js
+++ controllers/operationController.js
@@ -1,5 +1,6 @@
+const sequelize = require('../config/database');
 const { Op } = require('sequelize');
-const OperationProgressLog = require('../models/OperationProgressLog');
+const { OperationProgressLog } = require('../models');
 
 
 
